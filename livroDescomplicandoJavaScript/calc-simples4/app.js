006c6d9d5c34a465dde9c748c07e419c5f86420f
Odirlei Silva Leite <odirleite82@gmail.com>
2023-01-28 22:21:16 -0300

    Calculadora simples 4 + style

---
+++ livroDescomplicandoJavaScript/calc-simples4/app.js
@@ -0,0 +1,21 @@
+
+        function calcular(){
+            if (op.value === '+')
+                res.value = parseFloat(v1.value) + parseFloat(v2.value);
+            
+            else if (op.value === '-')
+                res.value = parseFloat(v1.value) - parseFloat(v2.value);
+
+            else if (op.value === '*')
+                res.value = parseFloat(v1.value) * parseFloat(v2.value);
+
+            else if (op.value === '/') {
+                if (parseFloat(v2.value) === 0)
+                    res.value = 'Erro!';
+                else
+                    res.value = parseFloat(v1.value) / parseFloat(v2.value);
+            }
+            else
+                alert('Escolha a operação desejada!');
+        }
+ 
